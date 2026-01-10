b4b887837736781bb990d94aed34aeeca9f30901
Jordi Ros López <jordi.ros@gracia.lasalle.cat>
2026-01-10 16:22:05 +0100

    feat: minimapa implementado

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
 @namespace
 class SpriteKind:
     Decorativo = SpriteKind.create()
-def CrearMapa():
-    scene.set_background_color(15)
-    tiles.set_current_tilemap(tilemap("""
-        nivel
-        """))
+    Map = SpriteKind.create()
 
-def on_right_pressed():
-    animation.run_image_animation(prota,
-        assets.animation("""
-            heroWalkRight
-            """),
-        100,
-        False)
-controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
+def on_a_pressed():
+    if prota.is_hitting_tile(CollisionDirection.BOTTOM):
+        prota.set_velocity(0, -150)
+controller.A.on_event(ControllerButtonEvent.PRESSED, on_a_pressed)
 
 def on_left_pressed():
     animation.run_image_animation(prota,
@@ -22,26 +14,67 @@ def on_left_pressed():
             heroWalkLeft
             """),
         100,
-        False)
+        True)
 controller.left.on_event(ControllerButtonEvent.PRESSED, on_left_pressed)
 
+def on_right_released():
+    animation.run_image_animation(prota,
+        assets.animation("""
+            heroWalkRight
+            """),
+        100,
+        False)
+controller.right.on_event(ControllerButtonEvent.RELEASED, on_right_released)
+
+def on_left_released():
+    animation.run_image_animation(prota,
+        assets.animation("""
+            heroWalkLeft
+            """),
+        100,
+        False)
+controller.left.on_event(ControllerButtonEvent.RELEASED, on_left_released)
+
+def on_right_pressed():
+    animation.run_image_animation(prota,
+        assets.animation("""
+            heroWalkRight
+            """),
+        100,
+        True)
+controller.right.on_event(ControllerButtonEvent.PRESSED, on_right_pressed)
+
+def CreacionMinimapa():
+    global myMinimap, mapStripe
+    myMinimap = minimap.minimap(MinimapScale.SIXTEENTH, 1, 15)
+    mapStripe = sprites.create(minimap.get_image(myMinimap), SpriteKind.Map)
 def CreacionPersonajes():
     global prota
     prota = sprites.create(assets.image("""
             ParadaPerfilDerecho
             """),
         SpriteKind.player)
+    controller.move_sprite(prota, 100, 0)
     prota.y = 460
-    prota.x = 60
-    controller.move_sprite(prota, 100, 100)
-    prota.ay = 200
+    prota.x = 40
     scene.camera_follow_sprite(prota)
+    prota.ay = 200
+mapStripe: Sprite = None
+myMinimap: minimap.Minimap = None
 prota: Sprite = None
-CrearMapa()
-antorcha = sprites.create(assets.image("""
-    miImagen
-    """), SpriteKind.player)
 info.set_life(3)
 nivel = 1
+tiles.set_current_tilemap(tilemap("""
+    nivel
+    """))
+CreacionMinimapa()
 CreacionPersonajes()
-posicionesAntorchas = [tiles.get_tile_location(10, 460)]
+
+def on_on_update():
+    pause(1)
+    sprites.destroy(mapStripe)
+    CreacionMinimapa()
+    minimap.include_sprite(myMinimap, prota, MinimapSpriteScale.DOUBLE)
+    mapStripe.set_position(scene.camera_property(CameraProperty.X) + 54,
+        scene.camera_property(CameraProperty.Y) - 44)
+game.on_update(on_on_update)
--- main.ts
+++ main.ts
@@ -1,16 +1,29 @@
 namespace SpriteKind {
     export const Decorativo = SpriteKind.create()
+    export const Map = SpriteKind.create()
 }
-controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (prota.isHittingTile(CollisionDirection.Bottom)) {
         prota.setVelocity(0, -150)
     }
 })
-function CrearMapa () {
-    scene.setBackgroundColor(15)
-    tiles.setCurrentTilemap(tilemap`nivel`)
-}
 controller.left.onEvent(ControllerButtonEvent.Pressed, function () {
+    animation.runImageAnimation(
+    prota,
+    assets.animation`heroWalkLeft`,
+    100,
+    true
+    )
+})
+controller.right.onEvent(ControllerButtonEvent.Released, function () {
+    animation.runImageAnimation(
+    prota,
+    assets.animation`heroWalkRight`,
+    100,
+    false
+    )
+})
+controller.left.onEvent(ControllerButtonEvent.Released, function () {
     animation.runImageAnimation(
     prota,
     assets.animation`heroWalkLeft`,
@@ -23,20 +36,33 @@ controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
     prota,
     assets.animation`heroWalkRight`,
     100,
-    false
+    true
     )
 })
+function CreacionMinimapa () {
+    myMinimap = minimap.minimap(MinimapScale.Sixteenth, 1, 15)
+    mapStripe = sprites.create(minimap.getImage(myMinimap), SpriteKind.Map)
+}
 function CreacionPersonajes () {
     prota = sprites.create(assets.image`ParadaPerfilDerecho`, SpriteKind.Player)
     controller.moveSprite(prota, 100, 0)
-    prota.setStayInScreen(true)
     prota.y = 460
-    prota.x = 60
+    prota.x = 40
     scene.cameraFollowSprite(prota)
-    prota.ay += 200
+    prota.ay = 200
 }
+let mapStripe: Sprite = null
+let myMinimap: minimap.Minimap = null
 let prota: Sprite = null
-CrearMapa()
 info.setLife(3)
 let nivel = 1
+tiles.setCurrentTilemap(tilemap`nivel`)
+CreacionMinimapa()
 CreacionPersonajes()
+game.onUpdate(function () {
+    pause(1)
+    sprites.destroy(mapStripe)
+    CreacionMinimapa()
+    minimap.includeSprite(myMinimap, prota, MinimapSpriteScale.Double)
+    mapStripe.setPosition(scene.cameraProperty(CameraProperty.X) + 54, scene.cameraProperty(CameraProperty.Y) - 44)
+})
